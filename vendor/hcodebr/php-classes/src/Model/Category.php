00501a7875e550b3b1af47945495b4047d914cab
Felippe <felippelealrr88@hotmail.com>
2023-06-26 19:04:34 -0400

    Site Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -30,6 +30,7 @@ class Category extends Model{
             //var_dump($results); exit;
             //seta no proprio objeto     
             $this->setData($results[0]);
+            Category::updateFile();
            
         }
 //==========================================================================================
@@ -55,7 +56,23 @@ public function deleteCategory(){
         ":idcategory"=>$this->getidcategory()
     ]);
 
+    Category::updateFile();
+
 }
+//================================================================================================
+
+public static function updateFile(){
+
+    $categories = Category::listAll();
+
+    $html = [];
+
+    foreach ($categories as $row) {
+        array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+    }
+    file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" .DIRECTORY_SEPARATOR. "categories-menu.html", implode('', $html));
+}
+
 
 }
 
