341454b29a959412ab88822720b64abbd8eb2895
Felippe <felippelealrr88@hotmail.com>
2023-07-05 19:25:48 -0400

    apagando detalhes

--- views-cache/categories-menu.ed528805425869f03885c566fea4f241.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Asus</a></li><li><a href="/categories/8">Microsoft</a></li><li><a href="/categories/9">Apple</a></li><li><a href="/categories/10">Samsung</a></li><li><a href="/categories/12">Morotolla</a></li><li><a href="/categories/13">Fortrek</a></li>
