4d67a638a391ff6da12ffb653abb7926a6c2f8dd
Felippe <felippelealrr88@hotmail.com>
2023-08-17 19:28:19 -0400

    Cep

--- site.php
+++ site.php
@@ -6,6 +6,9 @@ use \Hcode\Page;
 use \Hcode\Model\Category;
 use \Hcode\Model\Address;
 use Hcode\Model\User;
+use Hcode\Model\Order;
+use Hcode\Model\OrderStatus;
+
 
 //Rota do Home
 $app->get("/", function() { 
@@ -170,24 +173,149 @@ $app->post("/cart/freight", function(){
 
 });
 
-
+//checkput do carrinho
 $app->get("/checkout", function(){
-	
-	//Verifica se já está logado ou cadastrado
+
 	User::verifyLogin(false);
 
-	//Pega o carrinho na sessão
+	$address = new Address();
+	//pega o carrinho da sessão
 	$cart = Cart::getFromSession();
 
-	//Pega o endereço
-	$address = new Address();
+	//Se o cep não tá setado
+	if (!isset($_GET['zipcode'])) {
+		//pega o cep do carrinho
+		$_GET['zipcode'] = $cart->getdeszipcode();
+
+	}
+
+	//Se está setado
+	if (isset($_GET['zipcode'])) {
+		//carrega o objeto com o formato correto
+		$address->loadFromCEP($_GET['zipcode']);
+
+		$cart->setdeszipcode($_GET['zipcode']);
+
+		$cart->saveCart();
+		
+		//Força atualização do valor total
+		$cart->getCalculateTotal();
+
+	}
+
+	//Vefiricações
+	//Ficam definidos mesmo se vazios
+
+	if (!$address->getdesaddress()) $address->setdesaddress('');
+	if (!$address->getdesnumber()) $address->setdesnumber('');
+	if (!$address->getdescomplement()) $address->setdescomplement('');
+	if (!$address->getdesdistrict()) $address->setdesdistrict('');
+	if (!$address->getdescity()) $address->setdescity('');
+	if (!$address->getdesstate()) $address->setdesstate('');
+	if (!$address->getdescountry()) $address->setdescountry('');
+	if (!$address->getdeszipcode()) $address->setdeszipcode('');
 
 	$page = new Page();
 
+	//Passa os dados necessários ao template
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
-		'$address'=>$address->getValues()
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Address::getMsgError()
 	]);
+
+});
+
+$app->post("/checkout", function(){
+
+	//Usuario não é administrador
+	User::verifyLogin(false);
+
+	//Validações dos campos do formulário
+
+	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
+		Address::setMsgError("Informe o CEP.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === '') {
+		Address::setMsgError("Informe o endereço.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
+		Address::setMsgError("Informe o bairro.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['descity']) || $_POST['descity'] === '') {
+		Address::setMsgError("Informe a cidade.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
+		Address::setMsgError("Informe o estado.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
+		Address::setMsgError("Informe o país.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	//Pega o usuário da sessão
+	$user = User::getFromSession();
+
+	$address = new Address();
+
+	//Sobrescreve pq o campo no banco é diferente do template
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+
+	$address->setData($_POST);
+
+	$address->save();
+
+	//Pega o carrinho
+	$cart = Cart::getFromSession();
+
+	//Força o calculo do total
+	$cart->getCalculateTotal();
+
+	$order = new Order();
+
+	//Manda os dados para o template
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$cart->getvltotal()
+	]);
+
+	$order->save();
+
+	switch ((int)$_POST['payment-method']) {
+
+		case 1:
+		header("Location: /order/".$order->getidorder()."/pagseguro");
+		break;
+
+		case 2:
+		header("Location: /order/".$order->getidorder()."/paypal");
+		break;
+
+	}
+
+	exit;
+
 });
 
 $app->get("/login", function(){
